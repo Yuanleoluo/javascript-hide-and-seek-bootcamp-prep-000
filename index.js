ed5e3e9fd5b811341f75946bd3c7b77d86c9bedb
Yuanleoluo <leoyuanluo@gmail.com>
2017-10-13 03:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list')
   for (let i=0; i < lis.length; i++){
-    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML)+n).toString()
   }
 }
